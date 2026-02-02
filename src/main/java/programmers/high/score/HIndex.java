8496d301896db1f78d202fbbba85d12d158bde96
WithFortuna <rmsghchl0@naver.com>
2026-02-02 12:10:47 +0900

    20260202 - 정렬

--- src/main/java/programmers/high/score/HIndex.java
+++ src/main/java/programmers/high/score/HIndex.java
@@ -31,7 +31,6 @@ public class HIndex implements HighScoreKit{
 	class Solution {
 		public int solution(int[] citations) {
 			Arrays.sort(citations);
-
 			int h = 0;
 			for(int i = citations.length - 1; i >= 0; i--){
 				int tempH = citations.length - i;
